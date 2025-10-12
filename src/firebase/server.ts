f6f461d514c6af79c292c4bb82ae9c1c2b74722a
Saptrishi das biswas <saptrishi2019@gmail.com>
2025-10-12 14:25:53 +0000

    Error: Failed to parse private key: Error: Invalid PEM formatted message

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -15,7 +15,7 @@ const mockServiceKey = {
   "type": "service_account",
   "project_id": "wieblower",
   "private_key_id": "d0e1b69829f270034b7911e3b5311f0a2d3c4b5d",
-  "private_key": `-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/p+e5gX3x/kY2\n1f6qj9d7Z2p7x6eP/xT4bN7sZ9xY8l3d2a7n7...\n-----END PRIVATE KEY-----\n`,
+  "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/p+e5gX3x/kY2\\n1f6qj9d7Z2p7x6eP/xT4bN7sZ9xY8l3d2a7n7...\\n-----END PRIVATE KEY-----\\n",
   "client_email": "firebase-adminsdk-12345@wieblower.iam.gserviceaccount.com",
   "client_id": "123456789012345678901",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -31,11 +31,11 @@ if (serviceKey) {
     serviceAcct = JSON.parse(Buffer.from(serviceKey, 'base64').toString('utf8'));
   } catch(e) {
     console.error("Could not parse FIREBASE_SERVICE_ACCOUNT_KEY. Using mock key for local dev.", e)
-    serviceAcct = mockServiceKey;
+    serviceAcct = { ...mockServiceKey, private_key: mockServiceKey.private_key.replace(/\\n/g, '\n') };
   }
 } else {
   console.warn("FIREBASE_SERVICE_ACCOUNT_KEY is not set. Using mock key for local dev.");
-  serviceAcct = mockServiceKey;
+  serviceAcct = { ...mockServiceKey, private_key: mockServiceKey.private_key.replace(/\\n/g, '\n') };
 }
 
 if (!getApps().length) {
