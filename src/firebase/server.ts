6d7dec6d64603147796e63a535b6bd617ab669cc
Saptrishi das biswas <saptrishi2019@gmail.com>
2025-10-12 14:24:18 +0000

    Error: Failed to parse private key: Error: Invalid PEM formatted message

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,5 +1,7 @@
+'use server';
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
 import { firebaseConfig } from './config';
 
 // Load environment variables from a .env file
@@ -13,7 +15,7 @@ const mockServiceKey = {
   "type": "service_account",
   "project_id": "wieblower",
   "private_key_id": "d0e1b69829f270034b7911e3b5311f0a2d3c4b5d",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/p+e5gX3x/kY2\n1f6qj9d7Z2p7x6eP/xT4bN7sZ9xY8l3d2a7n7...\n-----END PRIVATE KEY-----\n",
+  "private_key": `-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/p+e5gX3x/kY2\n1f6qj9d7Z2p7x6eP/xT4bN7sZ9xY8l3d2a7n7...\n-----END PRIVATE KEY-----\n`,
   "client_email": "firebase-adminsdk-12345@wieblower.iam.gserviceaccount.com",
   "client_id": "123456789012345678901",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -36,7 +38,6 @@ if (serviceKey) {
   serviceAcct = mockServiceKey;
 }
 
-
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAcct),
@@ -45,3 +46,4 @@ if (!getApps().length) {
 }
 
 export const db = getFirestore();
+export const auth = getAuth();
